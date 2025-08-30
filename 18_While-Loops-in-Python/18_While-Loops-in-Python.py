81c82b527cdda076e5eab9d4c552ae6dfb58db4f
Moin-imrat <khanmoin4947@gmail.com>
2025-08-30 14:03:17 +0530

    30-8-25

---
+++ 18_While-Loops-in-Python/18_While-Loops-in-Python.py
@@ -0,0 +1,12 @@
+# First loop
+i = int(input("Enter your number: "))
+while(i<=40):
+     i = int(input("Enter your number: "))
+     print(i)
+print("loop is closed now")
+
+# Second loop 
+count = int(input("Enter your number: "))
+while(count>0):
+    print(count)
+    count=count-1
