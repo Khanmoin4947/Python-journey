85435b05137e7ecb3b431c041dd27f7861068651
Moin Khan <khanmoin4947@gmail.com>
2025-10-06 10:00:27 +0530

    More strings method are added

--- 13_string-methods/13_string-methods.py
+++ 13_string-methods/13_string-methods.py
@@ -6,6 +6,10 @@ print(name[0:3]) #(5,6 are the same)
 print(name[:3])
 
 print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name.replace())
 print(name.count("n"))
 print(name.endswith("n"))
 
