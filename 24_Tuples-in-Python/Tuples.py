daa02861c43859469ce3cbee35252883a30b4422
Moin Khan <khanmoin4947@gmail.com>
2025-10-23 20:56:55 +0530

    Tuples in python
    Today i learned about tuples in python

---
+++ 24_Tuples-in-Python/Tuples.py
@@ -0,0 +1,16 @@
+tup1=(1,2,3,"Moin")
+print(tup1)
+print(len(tup1))
+print(tup1[0])
+print(tup1[1])
+print(tup1[-1])
+
+
+if "Moin" in tup1:
+    print("yes")
+else:
+    print("nope")
+
+tup2=tup1[1:3]
+print(tup2)
+
